1079ce48fa01431a5011bd241898f56fbd28d473
mayank <mayank.gupta804@gmail.com>
2018-07-08 13:22:40 +0530

    ES6ify app.js and add an API for experimentation

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Musiclist Alpha' });
+    res.json({text: 'Musiclist API Text'})
 });
 
 module.exports = router;
